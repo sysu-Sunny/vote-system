65345ff4e298eeffc949346f5e405924e224cd9a
sysuzjz <a1098035@qq.com>
2014-04-23 15:44:15 +0800

    now it can add user and vote for them

---
+++ vote-system/main.js
@@ -0,0 +1,41 @@
+$("#main-content").on("click", ".arrow-up", function() {
+	var user = getUser(this);
+	if(isValidUser(user)) {
+		changeVotenum(this, 1);
+	} else {
+		alert("用户名非法");
+	}
+})
+
+$("#main-content").on("click", ".arrow-down", function() {
+	var user = getUser(this);
+	if(isValidUser(user)) {
+		changeVotenum(this, -1);
+	} else {
+		alert("用户名非法");
+	}
+})
+
+$("#main-content").on("click", "#add-user", function() {
+	$("#model").clone(true).removeClass('hide').insertBefore("#add-user");
+})
+
+function getUser(node) {
+	return $(node).siblings('.candidates').val();
+}
+
+function isValidUser(user) {
+	if(!user) {
+		return false;
+	}
+	return true;
+}
+
+function changeVotenum(node, num) {
+	var $numNode = $(node).siblings('.votenum');
+	var currentNum = parseInt($numNode.val());
+	currentNum += num;
+	if(currentNum >= 0) {
+		$numNode.val(currentNum + "票");
+	}
+}
