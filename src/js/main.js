8a7f44a49cc4d9f7993e41dbdbd21f6fa9e064e1
sysuzjz <a1098035@qq.com>
2014-04-23 17:36:26 +0800

    add the close button

--- src/js/main.js
+++ src/js/main.js
@@ -1,41 +1,45 @@
 $("#main-content").on("click", ".arrow-up", function() {
-	var user = getUser(this);
-	if(isValidUser(user)) {
-		changeVotenum(this, 1);
-	} else {
-		alert("用户名非法");
-	}
+    var user = getUser(this);
+    if(isValidUser(user)) {
+        changeVotenum(this, 1);
+    } else {
+        alert("用户名非法");
+    }
 })
 
 $("#main-content").on("click", ".arrow-down", function() {
-	var user = getUser(this);
-	if(isValidUser(user)) {
-		changeVotenum(this, -1);
-	} else {
-		alert("用户名非法");
-	}
+    var user = getUser(this);
+    if(isValidUser(user)) {
+        changeVotenum(this, -1);
+    } else {
+        alert("用户名非法");
+    }
 })
 
 $("#main-content").on("click", "#add-user", function() {
-	$("#model").clone(true).removeClass('hide').insertBefore("#add-user");
+    $("#model").clone(true).removeClass('hide').insertBefore("#add-user");
+})
+
+$("#main-content").on("click", ".btn-close", function() {
+    $(this).closest('.vote-box').remove();
 })
 
 function getUser(node) {
-	return $(node).siblings('.candidates').val();
+    return $(node).siblings('.candidates').val();
 }
 
 function isValidUser(user) {
-	if(!user) {
-		return false;
-	}
-	return true;
+    if(!user) {
+        return false;
+    }
+    return true;
 }
 
 function changeVotenum(node, num) {
-	var $numNode = $(node).siblings('.votenum');
-	var currentNum = parseInt($numNode.val());
-	currentNum += num;
-	if(currentNum >= 0) {
-		$numNode.val(currentNum + "票");
-	}
+    var $numNode = $(node).siblings('.votenum');
+    var currentNum = parseInt($numNode.val());
+    currentNum += num;
+    if(currentNum >= 0) {
+        $numNode.val(currentNum + "票");
+    }
 }
